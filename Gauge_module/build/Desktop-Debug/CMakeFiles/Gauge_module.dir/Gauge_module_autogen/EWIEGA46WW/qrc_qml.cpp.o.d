6b71e07ec6b6bfe0f9a4ee63b4848f2087b4fe99
JunSpring <wjdwnsqja08@gmail.com>
2024-09-18 14:39:43 +0200

    Remove Unnecessary Files

--- Gauge_module/build/Desktop-Debug/CMakeFiles/Gauge_module.dir/Gauge_module_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Gauge_module.dir/Gauge_module_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
-  /home/yeongyoo/DES_Instrument-Cluster-Team7/Gauge_module/build/Desktop-Debug/Gauge_module_autogen/EWIEGA46WW/qrc_qml.cpp
--- Gauge_module/build/Desktop-Debug/CMakeFiles/Gauge_module_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Gauge_module_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Gauge_module_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
